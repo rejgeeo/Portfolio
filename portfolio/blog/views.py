3b59c66925ffd40e768a97c8e4186038808d5216
rejgeeo <rejoyg@gmail.com>
2018-10-01 16:47:43 +0530

    portfolio site completed

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . models import Blog
 
 # Create your views here.
+def allblogs(request):
+    blogs = Blog.objects
+    return render(request,'blogs/allblogs.html', {'blogs':blogs})
+
+
+def blogdetail(request,blog_id):
+    myblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/blogdetail.html', {'myblog':myblog})
